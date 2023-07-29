b4c1c805c1f3ddb56bf4fbc83c0564c859b9ac12
“yangchunhua” <1250855168@qq.com>
2023-07-29 13:59:24 +0800

    学习一点小语法

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -1,57 +1,55 @@
 /******************************
-*   author: yuesong-feng
-*   
-*
-*
-******************************/
+ *   author: yuesong-feng
+ *
+ *
+ *
+ ******************************/
 #pragma once
+#include <condition_variable>
 #include <functional>
-#include <vector>
+#include <future>
+#include <mutex>
 #include <queue>
 #include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <future>
+#include <vector>
 
-class ThreadPool
-{
+class ThreadPool {
 private:
-    std::vector<std::thread> threads;
-    std::queue<std::function<void()>> tasks;
-    std::mutex tasks_mtx;
-    std::condition_variable cv;
-    bool stop;
+  std::vector<std::thread> threads;
+  std::queue<std::function<void()>> tasks;
+  std::mutex tasks_mtx;
+  std::condition_variable cv;
+  bool stop;
+
 public:
-    ThreadPool(int size = 10);
-    ~ThreadPool();
+  ThreadPool(int size = 10);
+  ~ThreadPool();
 
-    // void add(std::function<void()>);
-    template<class F, class... Args>
-    auto add(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>;
-
+  // void add(std::function<void()>);
+  template <class F, class... Args>
+  auto add(F &&f, Args &&...args)
+      -> std::future<typename std::result_of<F(Args...)>::type>;
 };
 
-
 //不能放在cpp文件，原因是C++编译器不支持模版的分离编译
-template<class F, class... Args>
-auto ThreadPool::add(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type> {
-    using return_type = typename std::result_of<F(Args...)>::type;
+template <class F, class... Args>
+auto ThreadPool::add(F &&f, Args &&...args)
+    -> std::future<typename std::result_of<F(Args...)>::type> {
+  using return_type = decltype(f(args...));
 
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-        
-    std::future<return_type> res = task->get_future();
-    {
-        std::unique_lock<std::mutex> lock(tasks_mtx);
+  auto task = std::make_shared<std::packaged_task<return_type()>>(
+      std::bind(std::forward<F>(f), std::forward<Args>(args)...));
 
-        // don't allow enqueueing after stopping the pool
-        if(stop)
-            throw std::runtime_error("enqueue on stopped ThreadPool");
+  std::future<return_type> res = task->get_future();
+  {
+    std::unique_lock<std::mutex> lock(tasks_mtx);
 
-        tasks.emplace([task](){ (*task)(); });
-    }
-    cv.notify_one();
-    return res;
+    // don't allow enqueueing after stopping the pool
+    if (stop)
+      throw std::runtime_error("enqueue on stopped ThreadPool");
+
+    tasks.emplace([task]() { (*task)(); });
+  }
+  cv.notify_one();
+  return res;
 }
